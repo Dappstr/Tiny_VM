6f691d8d80385073ddba30e707a062b059c4d6ad
Lane Beals <lanebeals02@gmail.com>
2023-08-30 17:10:53 -0700

    Changed file util to not use stringstream

--- src/fileutil.cpp
+++ src/fileutil.cpp
@@ -1,20 +1,37 @@
 #include <fstream>
-#include <sstream>
+//#include <sstream>
 #include <string>
 #include <iostream>
 
 #include "../include/fileutil.hpp"
 
 std::string get_file_contents(const char* path) {
+    if(!path) {
+        std::cerr << "Failed getting file's path\n";
+        exit(EXIT_FAILURE);
+    }
+
     std::fstream file(path, std::ios::in);
+    
     if(!file.is_open()) {
         std::cerr << "Failed to open file\n";
         exit(EXIT_FAILURE);
     }
 
+    file.seekg(0, std::ios::end);
+    size_t size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::string contents(size, '\0');
+    if(!file.read(&contents[0], size)) {
+        throw std::runtime_error("Failed to read file!\n");
+    }
+
+    /*
     std::stringstream content_stream;
     content_stream << file.rdbuf();
 
     std::string contents = content_stream.str();
+    */
     return contents;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,16 @@ int main(int argc, char* argv[]) {
 
     //get file contents
     char* path = argv[1];
-    std::string contents = get_file_contents(path);
-
+    std::string source;
+    try {
+        std::string contents = get_file_contents(path);
+        source = contents;
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what();
+    }
     //lex
-    std::vector<std::string> lexemes = lex(contents);
+    std::vector<std::string> lexemes = lex(source);
     
     //tokenize
     std::vector<Token> tokens = tokenize(lexemes);
